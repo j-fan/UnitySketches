82a892fb87de24f069099a9fc523540850909e0c
JF <jfan9988@gmail.com>
2019-04-28 17:09:43 +1000

    add OSC controls

---
+++ Assets/Scripts/OSCReceive.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OSCReceive : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public OSC osc;
+    void Start()
+    {
+        osc.SetAddressHandler("/midi/mixtrack_pro_3/0/3/control_change", OnGetMessage);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnGetMessage(OscMessage m){
+        print(m);
+    }
+}
--- Assets/Scripts/StrangeAttractors.cs
+++ Assets/Scripts/StrangeAttractors.cs
@@ -15,6 +15,7 @@ public class StrangeAttractors : MonoBehaviour
     public AttractorType attractorType;
     public ForceType forceType;
     public BeatsFFT beatsFFT;
+    public OSC osc;
 
     private ParticleSystem particleSys;
     private ParticleSystem.MainModule particleSysMain;
@@ -22,9 +23,11 @@ public class StrangeAttractors : MonoBehaviour
     private ParticleSystem.TrailModule particleTrailModule;
     private StrangeAttractor strangeAttractor;
     private ForceAttractor forceAttractor;
+    private float speedModifier = 1.0f;
     
     private void Start()
     {
+        //osc.SetAddressHandler("/midi/mixtrack_pro_3/0/3/control_change", OnRightFilter);
         forceAttractor = new ForceAttractor(null);
         strangeAttractor = new StrangeAttractor();
         particleSys = GetComponent<ParticleSystem>();
@@ -47,6 +50,13 @@ public class StrangeAttractors : MonoBehaviour
         }
     }
 
+    private void OnRightFilter(OscMessage message)
+    {
+        int id = Int32.Parse(message.values[0].ToString(), System.Globalization.NumberStyles.HexNumber);
+        int value = Int32.Parse(message.values[1].ToString(), System.Globalization.NumberStyles.HexNumber);
+        speedModifier = (float)value / 128f;
+        // print(message.values[1]);
+    }
     private void LateUpdate()
     {
         int maxParticles = particleSysMain.maxParticles;
@@ -60,6 +70,7 @@ public class StrangeAttractors : MonoBehaviour
             // particles[i].velocity = forceAttractor.Apply(forceType, particles[i].position, particles[i].velocity);
             particles[i].velocity = strangeAttractor.Apply(attractorType, particles[i].position);
             particles[i].velocity = Vector3.ClampMagnitude(Vector3.Normalize(particles[i].velocity) * 1000 * beatsFFT.avgFreq,10);
+            particles[i].velocity = particles[i].velocity * speedModifier * 4f;
             // particles[i].velocity = Vector3.Normalize(particles[i].velocity);
 
             // prevent particles going to infinity
@@ -69,7 +80,8 @@ public class StrangeAttractors : MonoBehaviour
         }
         // save modified particles
         particleSys.SetParticles(particles,particles.Length);
-        transform.Rotate(30 * beatsFFT.avgFreq, 40 * beatsFFT.avgFreq, 30 * beatsFFT.avgFreq, Space.World);
+        //transform.Rotate(30 * beatsFFT.avgFreq, 40 * beatsFFT.avgFreq, 30 * beatsFFT.avgFreq, Space.World);
+        transform.Rotate(speedModifier,0,speedModifier);
         particleTrailModule.colorOverTrail = new Color(1f, 1f, 1f, (beatsFFT.runningAvgFreq * 10) + 0.01f);
     }
 
